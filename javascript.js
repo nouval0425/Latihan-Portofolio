d9a2ebdbbb2c8ac11257d5db33305bf650b97718
nouval0425 <ghifarynopal@gmail.com>
2025-12-06 13:26:36 +0700

    js

---
+++ javascript.js
@@ -0,0 +1,37 @@
+//untuk tombol mode gelap
+let toggle=document.getElementById("toggleDark")
+
+toggle.addEventListener("click",()=>{
+    body.classList.toggle("dark-mode")
+
+    if (body.classList.contains("dark-mode")){
+        toggle.textContent ="☀️"
+    }
+    else{
+toggle.textContent="🌙"
+    }
+    
+})
+
+//image slider
+let next;
+let index =0
+let image =document.getElementById("slider")
+let gambar=[
+"me.jpeg",
+"instagram.jpg"
+]
+
+function update(){
+    index++;
+
+if(index >= gambar.length){
+    index = 0;
+}
+image.src=gambar[index];
+
+}
+setInterval(update,3000)
+
+
+
